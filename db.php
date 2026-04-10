2ff8560af2e302f93e85f12e60b7336435640da2
numericstudio <numeric.creativestudio@gmail.com>
2026-04-10 22:28:09 +0100

    Update db.php

--- db.php
+++ db.php
@@ -8,7 +8,7 @@ $port = "3306";
 try {
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected!";
+  //  echo "Connected!";
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
